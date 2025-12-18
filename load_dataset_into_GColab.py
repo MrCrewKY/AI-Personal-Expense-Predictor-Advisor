85f15f6a214465b61ff3d229c25e3e1150eefe87
Henry Lim <limhenry0065@gmail.com>
2025-12-18 10:07:56 +0800

    Update and rename load_dataset_into_GColab.ipynb to load_dataset_into_GColab.py

--- load_dataset_into_GColab.py
+++ load_dataset_into_GColab.py
@@ -1,5 +1,7 @@
+# Need to install this when run session every time
 !pip install pandas numpy scikit-learn prophet tensorflow matplotlib
 import pandas as pd
 
+# Upload csv file in folder named content in Google Colab
 df = pd.read_csv("Personal_Finance_Dataset.csv")
 df.head()
